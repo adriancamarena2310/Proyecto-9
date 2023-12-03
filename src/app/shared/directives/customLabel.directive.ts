67aca245fbb86f6e6d8b48819ce24dba5dc78288
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 08:16:42 -0600

    video-319 Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/customLabel.directive.ts
+++ src/app/shared/directives/customLabel.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -7,8 +8,8 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
-
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color( value: string ) {
     this._color = value;
@@ -22,6 +23,11 @@ export class CustomLabelDirective implements OnInit{
 
   }
 
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
+
   ngOnInit(): void {
     this.setStyle();
   }
@@ -31,5 +37,19 @@ export class CustomLabelDirective implements OnInit{
     if ( !this.htmlElement )return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage():void {
+    if ( !this.htmlElement )return;
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    console.log(errors)
+    if ( errors.includes('required') )  {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+      return;
+    }
   }
 }
