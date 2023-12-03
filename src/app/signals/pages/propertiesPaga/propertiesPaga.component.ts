552e41e40493decdb1defe827f6fb335bedf569b
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 09:39:38 -0600

    video-332 Mutaciones - Actualizar parte del estado

--- src/app/signals/pages/propertiesPaga/propertiesPaga.component.ts
+++ src/app/signals/pages/propertiesPaga/propertiesPaga.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-propertiesPaga',
@@ -12,7 +13,57 @@ export class PropertiesPagaComponent implements OnInit {
   ngOnInit() {
   }
 
-  onFieldUpdated(field: string,value:string){
-    console.log({field,value})
+
+  public user = signal<User>( {
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  } );
+
+
+  onFieldUpdated( field: keyof User, value: string ) {
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    // this.user.update( current => ({
+    //   ...current,
+    //   [field]: value
+    // }))
+
+    this.user.update( current => {
+
+      switch ( field ) {
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number( value );
+        break;
+      }
+
+
+      return current;
+    } );
+
+
   }
 }
