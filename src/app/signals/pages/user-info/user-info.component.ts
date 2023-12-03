508875a0296711c08b8db808d5be4461b0a7090c
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 09:20:21 -0600

    video-329 Señales - Información del usuario

--- src/app/signals/pages/user-info/user-info.component.ts
+++ src/app/signals/pages/user-info/user-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
+import { UserServiceService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-info',
@@ -9,7 +11,28 @@ export class UserInfoComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  private usersService = inject(UserServiceService);
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() )
   }
 
+  loadUser( id: number ) {
+    if ( id <= 0 ) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById( id )
+      .subscribe({
+        next: (user) => {
+         this.currentUser.set( user );
+
+        }
+      });
+    }
 }
